b89136ab0d61a3d9c7638297f61f1beaeaa19eef
Christoffer Vig <christoffer.vig@comperiosearch.com>
2015-11-03 18:21:49 +0100

    added readme and reading location of my documents from environment

---
+++ testingtika/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Nest;
+using TikaOnDotNet;
+//Simple test of indexing documents into Elasticsearch 
+//using Apache Tika wrapped by the TikoOnDotNet project 
+//and the NEST elasticsearch client library for C#
+namespace testingtika
+{
+    //POCO for the formatting of the items to feed into Elasticsearch
+    public class Article
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [ElasticProperty(Index = FieldIndexOption.Analyzed, TermVector = TermVectorOption.WithPositionsOffsets)]
+        public string Body { get; set; }
+        public string ContentType { get; set; }
+        public long ContentLength { get; set; }
+    }
+
+    class Program
+    {
+
+        private static TextExtractor _cut;
+        private static string _IndexName = "files";
+
+        private static ElasticClient elastic()
+        {
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(
+                node,
+                defaultIndex: _IndexName
+            );
+            var client = new ElasticClient(settings);
+            return client;
+        }
+
+
+        // Extract content and metadata from the file.
+        // put the information into an Article object
+        private static Article tika(string filename)
+        {
+
+            var result = _cut.Extract(filename);
+            var article = new Article()
+            {
+                Id = filename,
+                ContentType = result.ContentType,
+                Body = result.Text
+            };
+            if (result.Metadata.ContainsKey("title"))
+                article.Title = result.Metadata["title"];
+            else
+                article.Title = filename;
+            if (result.Metadata.ContainsKey("Content-Length"))
+            {
+                long longtest;
+                if (long.TryParse(result.Metadata["Content-Length"], out longtest))
+                {
+                    article.ContentLength = longtest;
+                }
+            }
+            if (result.Metadata.ContainsKey("Author"))
+                article.Author = result.Metadata["Author"];
+            return article;
+        }
+
+        static void Main(string[] args)
+        {
+            string currentDir = "";
+            if (args.Length == 0)
+            {
+                currentDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            } else
+                currentDir = args[0];
+            if (args.Length > 1)
+                _IndexName = args[1];
+            var files = System.IO.Directory.GetFiles(currentDir);
+            _cut = new TextExtractor();
+            var es = elastic();
+            es.CreateIndex(_IndexName, ind => ind.Index(_IndexName)
+                .AddMapping<Article>(t => t.MapFromAttributes()));
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    var test = tika(file);
+                    var indexresult = es.Index<Article>(test);
+                    Console.WriteLine(indexresult.Id);
+                }
+                catch (Exception e)
+                {
+                    
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
+
+        }
+    }
+}
---
+++ testingtika/readme.md
@@ -0,0 +1,22 @@
+﻿# Demo console app for indexing windows files with elasticsearch
+## What is it?
+A simple test program showing how to use TikaOnDotNet to extract metadata from files on a windows file system, and index them into Elasticsearch.
+## How can I use it?
+Clone the repo, and open the program in visual Studio. Running the program will read all documents from your "my documents"
+folder, and index into Elasticsearch.
+If you want to search the files you indexed, you can for instance use Sense
+ ( automagically installed by the below-mentioned ansible vagrant box at [http://localhost:5601/app/sense](http://localhost:5601/app/sense)
+Or, if no sense available, you should see something on this [http://localhost:9200/files/_search](http://localhost:9200/files/_search)
+
+The program can even read input arguments - the folder to read files from, and the indexname to use in Elasticsearch
+## But why doesn't it...?
+Hey, this is a *simple* demo. Emphasis on simple. If you need something, do it yourself.
+##Requirements
+####Windows
+This is a windows program
+####Visual Studio
+You most likely want to run this program in visual Studio. I used version 2013
+####Elasticsearch
+Requires elasticsearch running on localhost at port 9200. Tested with Elasticsearch 2.0, but it likely runs with even older versions. 
+
+(you can install elasticsearch in a VM using  [https://github.com/comperiosearch/vagrant-elk-box-ansible](https://github.com/comperiosearch/vagrant-elk-box-ansible)))
